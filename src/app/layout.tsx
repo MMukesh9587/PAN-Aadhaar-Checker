66da05ec39dad9e3c33b29f41e96b246a364580e
Mukesh Yogi <mukeshyogi7766@gmail.com>
2025-11-08 06:27:08 +0000

    HTML tag add karo (<meta name="google-site-verification" content="oH3pK0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="oH3pK091kHNpTU7FK5KRyYZNSLI5DG4OR25KocEWyxo" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
